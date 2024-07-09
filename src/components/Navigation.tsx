65c6eabe04418b25d033bdb1b9f9d73f709fa779
Guillermo Lescano <guillelescano.gl@gmail.com>
2024-07-09 16:06:39 -0300

    creacion base de componentes navigation y footer

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
+import 'primereact/resources/themes/saga-blue/theme.css';  // Tema de PrimeReact
+import 'primereact/resources/primereact.min.css';          // Estilos principales de PrimeReact
+import 'primeicons/primeicons.css';                        // Iconos de PrimeReact
+
+type Props = {}
+
+const Navigation = (props: Props) => {
+    const items: MenuItem[] = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Components',
+                    icon: 'pi pi-bolt'
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil'
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ];
+    
+    return (
+        <div className="card">
+            <Menubar model={items} />
+        </div>
+    )
+}
+
+export default Navigation
